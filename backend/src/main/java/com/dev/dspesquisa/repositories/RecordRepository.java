7bcb992fff791b219bf06bfb39e8b48ea08bec31
Renan Catto <renan.catto@gmail.com>
2020-12-17 23:15:54 -0300

    Realizado a implementação do DTO e Service

--- backend/src/main/java/com/dev/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/dev/dspesquisa/repositories/RecordRepository.java
@@ -1,10 +1,11 @@
 package com.dev.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dev.dspesquisa.entities.Record;
 
-
+@Repository
 public interface RecordRepository extends JpaRepository<Record, Long>{
 
 }
---
+++ backend/src/main/java/com/dev/dspesquisa/services/GameService.java
@@ -0,0 +1,25 @@
+package com.dev.dspesquisa.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dev.dspesquisa.dto.GameDTO;
+import com.dev.dspesquisa.entities.Game;
+import com.dev.dspesquisa.repositories.GameRepository;
+
+@Service
+public class GameService {
+	
+	@Autowired
+	private GameRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<GameDTO> findAll(){
+		List <Game> list = repository.findAll();
+		return list.stream().map(x -> new GameDTO(x)).collect(Collectors.toList());
+	}
+}
