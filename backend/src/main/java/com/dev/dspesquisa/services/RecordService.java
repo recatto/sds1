28cda2a89eb4b12df82fa1f9b1e0869eeee05921
Renan Catto <renan.catto@gmail.com>
2020-12-21 20:57:01 -0300

    post de record

---
+++ backend/src/main/java/com/dev/dspesquisa/services/RecordService.java
@@ -0,0 +1,40 @@
+package com.dev.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dev.dspesquisa.dto.RecordDTO;
+import com.dev.dspesquisa.dto.RecordInsertDTO;
+import com.dev.dspesquisa.entities.Game;
+import com.dev.dspesquisa.entities.Record;
+import com.dev.dspesquisa.repositories.GameRepository;
+import com.dev.dspesquisa.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+	
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		Record entity = new Record();
+		
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		
+		return new RecordDTO(entity);
+	}
+}
